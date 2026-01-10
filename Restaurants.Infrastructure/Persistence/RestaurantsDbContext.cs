209f963bcb8b25ec87c6b0638631c4876218981f
khomma0312 <kijikuroneko@gmail.com>
2026-01-10 14:47:54 +0900

    DBの接続設定をapplication.Development.jsonに移動、プレゼンテーション層にDbContextのDIを追加

--- Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -4,15 +4,15 @@ using Restaurants.Domain.Entities;
 
 namespace Restaurants.Infrastructure.Persistence;
 
-internal class RestaurantsDbContext: DbContext
+internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options) : DbContext(options)
 {
   internal DbSet<Restaurant> Restaurants { get; set;  }
   internal DbSet<Dish> Dishes { get; set;  }
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    optionsBuilder.UseSqlServer("Server=localhost,1433;Database=RestaurantsDb;User Id=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=True;Encrypt=false");
-  }
+  // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  // {
+  //   optionsBuilder.UseSqlServer("Server=localhost,1433;Database=RestaurantsDb;User Id=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=True;Encrypt=false");
+  // }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
